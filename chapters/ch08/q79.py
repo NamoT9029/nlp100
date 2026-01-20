c8560d950165715c2556fabc24fde5bc94faf3a3
NamoT9029 <238596706+NamoT9029@users.noreply.github.com>
2026-01-20 10:42:54 +0900

    ch08 add q79.py

--- chapters/ch08/q79.py
+++ chapters/ch08/q79.py
@@ -7,6 +7,7 @@ from torch.utils.data import DataLoader
 import torch.nn as nn 
 from dotenv import load_dotenv
 import os
+import torch.nn.functional as F
 
 load_dotenv()
 PROJECT_ROOT = os.getenv('PROJECT_ROOT')
@@ -43,14 +44,67 @@ class LSTM(nn.Module):
         z = self.sigmoid(y)
         return z
 
+class CNN(nn.Module):
+    def __init__(self, input_size, output_size=1, batch_size=64, embedding=None, freeze=True):
+        super().__init__()
+        self.linear= nn.Linear(batch_size * 3, output_size)
+        self.sigmoid = nn.Sigmoid()
+        self.embedding = nn.Embedding.from_pretrained(embedding, freeze=freeze)
+        self.conv1 = nn.Conv2d(1, batch_size, kernel_size=(2, input_size))
+        self.conv2 = nn.Conv2d(1, batch_size, kernel_size=(4, input_size))
+        self.conv3 = nn.Conv2d(1, batch_size, kernel_size=(8, input_size))
+        self.batch_size = batch_size
+
+    def forward(self, input_ids):
+        emb = self.embedding(input_ids)
+        out = emb.unsqueeze(1)
+
+        out1 = F.relu(self.conv1(out))
+        out2 = F.relu(self.conv2(out))
+        out3 = F.relu(self.conv3(out))
+        
+        out1 = F.max_pool2d(out1, kernel_size=(out1.size()[2], 1))
+        out2 = F.max_pool2d(out2, kernel_size=(out2.size()[2], 1))
+        out3 = F.max_pool2d(out3, kernel_size=(out3.size()[2], 1))
+        
+        out1 = out1.view(-1, self.batch_size)
+        out2 = out2.view(-1, self.batch_size)
+        out3 = out3.view(-1, self.batch_size)
+
+        out = torch.cat([out1, out2, out3], dim=1)
+
+        out = self.linear(out)
+        out = self.sigmoid(out)
+
+        return out
+
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size=128, output_size=1, embedding=None, freeze=True):
+        super().__init__()
+        self.linear= nn.Linear(hidden_size, output_size)
+        self.sigmoid = nn.Sigmoid()
+        self.embedding = nn.Embedding.from_pretrained(embedding, freeze=freeze)
+        self.rnn = nn.RNN(input_size=input_size, hidden_size=hidden_size, batch_first=True)
+
+    def forward(self, input_ids):
+        emb = self.embedding(input_ids)
+        _, h = self.rnn(emb)
+        x = h[-1]
+        y = self.linear(x)
+        z = self.sigmoid(y)
+        return z
+
+
 def main():
     epochs = 20
     lr = 0.01
     emb_ts = torch.tensor(emb)
-    model = LSTM(input_size=vector_size, hidden_size=128, output_size=1, embedding=emb_ts, freeze=False).to(device)
+    batch_size = 256
+    # model = LSTM(input_size=vector_size, hidden_size=128, output_size=1, embedding=emb_ts, freeze=False).to(device)
+    # model = CNN(input_size=vector_size, output_size=1, batch_size=batch_size, embedding=emb_ts, freeze=False).to(device)
+    model = RNN(input_size=vector_size, hidden_size=128, output_size=1, embedding=emb_ts, freeze=False).to(device)
     loss_fn = nn.BCELoss()
     optimizer = torch.optim.Adam(model.parameters(),lr=lr)
-    batch_size = 256
 
     train_ds = MyDataset(train)
     dev_ds = MyDataset(dev)
